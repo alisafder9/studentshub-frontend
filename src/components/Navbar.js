324dbc276efd806f0b5b9be1723ca77ac4a7ba68
Safdar Ali Ihsan <147537936+alisafder9@users.noreply.github.com>
2025-03-10 02:37:53 +0500

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,7 +20,7 @@ const Navbar = () => {
       try {
         const token = localStorage.getItem('token') || sessionStorage.getItem('token');
 
-        const response = await fetch(`http://localhost:5000/api/users/name?email=${email}`, {
+        const response = await fetch(`https://studentshub-backend.vercel.app/api/users/name?email=${email}`, {
           method: 'GET',
           headers: {
             'Authorization': `Bearer ${token}`,
