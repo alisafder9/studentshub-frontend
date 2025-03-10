43af57eb8e0a542c5e1974732eae74f1c46464df
Safdar Ali Ihsan <147537936+alisafder9@users.noreply.github.com>
2025-03-11 00:37:51 +0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -39,7 +39,7 @@ const Login = () => {
 
   return (
     <div className='login-container'>
-      <div className="container mt-5">
+      <div className="container">
         <div className="row justify-content-center">
           <div className="col-md-6">
             <div className="card">
